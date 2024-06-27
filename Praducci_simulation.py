b5c4a7a7b2d8cb28bfe8c83a6dac6dd436f71db6
AmitShirazi1 <amit.shirazi@campus.technion.ac.il>
2024-06-27 20:50:47 +0300

    Basic idea is built

--- Praducci_simulation.py
+++ Praducci_simulation.py
@@ -3,6 +3,7 @@ import networkx as nx
 import random
 import pandas as pd
 import csv
+import matplotlib.pyplot as plt
 
 ID1, ID2 = 319044434, 314779166
 # influencers = [555]
@@ -29,6 +30,47 @@ def create_graph(edges_path: str) -> nx.Graph:
     return net
 
 
+def independent_cascade(G, initial_active):
+    active = set(initial_active)
+    new_active = set(initial_active)
+    
+    while new_active:
+        current_active = set(new_active)
+        new_active = set()
+        for node in current_active:
+            neighbors = set(G.neighbors(node)) - active
+            for neighbor in neighbors:
+                if random.random() < 0.5:  # Probability of influencing
+                    new_active.add(neighbor)
+                    active.add(neighbor)
+    return active
+
+
+def total_influencer_cost(influencers, costs):
+    return sum(costs.get(influencer, 0) for influencer in influencers)
+
+def is_within_budget(influencers, budget):
+    return total_influencer_cost(influencers, costs) <= budget
+
+
+class Node:
+    def __init__(self, id, num_neighbors, expected_num_bought_neighbors=[0]*6):
+        self.id = id
+        self.cost = None
+        self.num_neighbors = num_neighbors
+        self.expected_num_bought_neighbors = expected_num_bought_neighbors
+    
+    def get_buying_prob(self, t):
+        return self.expected_num_bought_neighbors[t] / self.num_neighbors
+    
+    def add_neighbor_to_buyers(self, t):
+        self.expected_num_bought_neighbors[t] += 1
+
+    def set_cost(self, cost):
+        self.cost = cost
+
+
+
 def choose_influencers(NoseBook_network, ordered_nodes):
     """
     Choose the influencers you want to work with
@@ -38,22 +80,49 @@ def choose_influencers(NoseBook_network, ordered_nodes):
     # the total cost of the influencers should not exceed 1000
     money_left = 1000
     influencers = []
-    while money_left > 0:
-        influencer = ordered_nodes.pop(0)
+    neigh_to_remove = set()
+    i = 0
+    while money_left > 0 and i < len(ordered_nodes):
+        influencer = ordered_nodes[i]
+        i += 1
         # while influencer in influencers:
         #     influencer = random.choice(list(NoseBook_network.nodes))
         while get_influencers_cost(cost_path, influencers + [influencer]) > 1000:
-            influencer = ordered_nodes.pop(0)
-            print(" expensive influencer: ", influencer)
+            influencer = ordered_nodes[i]
+            i += 1
+            print("Expensive influencer: ", influencer)
         influencers.append(influencer)
-        neighbors = [influencer]
-        for _ in range(6):
-            for node in neighbors:
-                neighbors += list(NoseBook_network.neighbors(node))
-                print("")
-        ordered_nodes = [node for node in ordered_nodes if node not in neighbors]
+
+        curr_neighbors = {influencer}
+        nodes_created = dict()
+        # Simulating 6 rounds of buying
+        for t in range(6):
+            neighbors = set()
+            curr_neighbors = curr_neighbors.difference(neigh_to_remove)  # Remove the neighbors that were already bought
+            neigh_to_remove = neigh_to_remove.union(curr_neighbors)  # Add the neighbors that were already bought
+
+            # Update the neighbors of the current neighbors
+            for neigh in curr_neighbors:
+                curr_node_neighs = set(NoseBook_network.neighbors(neigh))
+                neighbors = neighbors.union(curr_node_neighs)  # Add the neighbors of the current neighbors
+                # Create a node for each neighbor
+                if neigh not in nodes_created.keys():
+                    node = Node(neigh, len(curr_node_neighs))
+                    nodes_created[neigh] = node
+                else:
+                    node = nodes_created[neigh]
+                    node.add_neighbor_to_buyers(t)
+
+                
+            print("Updated neighbors: ", neighbors)
+            curr_neighbors = neighbors  # Add bernoulli according to the probability of buying
+            # Also, think about maybe we want duplicates.
+
+
+        ordered_nodes = [node for node in ordered_nodes if node not in neigh_to_remove]
         money_left -= get_influencers_cost(cost_path, influencers)
         print("Money left: ", money_left)
+
     return influencers
 
 
@@ -113,10 +182,31 @@ if __name__ == '__main__':
     print("STARTING")
 
     NoseBook_network = create_graph(NoseBook_path)
-    # calculate the degree centrality of each node in the network
-    degree_centrality = [key for key, _ in sorted(nx.degree_centrality(NoseBook_network).items(), key=lambda item: item[1], reverse=True)]
-    print("Degree centrality: ", degree_centrality)
-    print(type(degree_centrality))
+    # # Draw the network
+    # pos = nx.random_layout(NoseBook_network)  # positions for all nodes
+    # nx.draw(NoseBook_network, pos, with_labels=False, node_size=10, width=0.01, alpha=0.5, node_color='teal')
+    # plt.show()
+    
+    costs = pd.read_csv(cost_path)
+    costs = dict(zip(costs['user'], costs['cost']))
+
+
+    #sort the nodes by degree centrality and influencers cost of this node
+    degree_centrality = nx.degree_centrality(NoseBook_network)
+    # Normalize and compute cost-effectiveness
+    cost_effectiveness = {node:degree_centrality[node] /0.1* costs[node]  if node in costs and costs[node] != 0 else 0 for node in NoseBook_network.nodes}
+
+    # Sort nodes based on cost-effectiveness
+    sorted_nodes = sorted(cost_effectiveness.keys(), key=lambda x: cost_effectiveness[x], reverse=True)
+
+    #sorted_nodes = sorted(NoseBook_network.nodes, key = lambda n: degree_centrality[n]/costs[costs['user'] == n]['cost'].iloc[0], reverse=True)
+
+
+    # # calculate the degree centrality of each node in the network
+    # degree_centrality = [key for key, _ in sorted(nx.degree_centrality(NoseBook_network).items(), key=lambda item: item[1], reverse=True)]
+    # # TODO: divide by the cost.
+    print("sorted_nodes: ", sorted_nodes)
+    print(type(sorted_nodes))
     # .sort_values(ascending=False).keys()
     # calculate the closeness centrality of each node in the network
     # closeness_centrality = nx.closeness_centrality(NoseBook_network).sort_values(ascending=False).keys()
@@ -124,12 +214,19 @@ if __name__ == '__main__':
     # betweenness_centrality = nx.betweenness_centrality(NoseBook_network).sort_values(ascending=False).keys()
     # calculate the eigenvector centrality of each node in the network
     
+    # Select influencers within budget
+    budget = 1000
+    influencers = []
+    current_budget = budget
+    for node in sorted_nodes:
+        if costs[node] <= current_budget:
+            influencers.append(node)
+            current_budget -= costs[node]
+        if current_budget <= 0:
+            break    
 
-
-
-
-
-    influencers = choose_influencers(NoseBook_network, list(degree_centrality))
+    #influencers = choose_influencers(NoseBook_network, list(sorted_nodes))
+    print("Influencers: ", influencers)
     influencers_cost = get_influencers_cost(cost_path, influencers)
     print("Influencers cost: ", influencers_cost)
     if influencers_cost > 1000:
